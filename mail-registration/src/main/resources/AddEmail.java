f17327b74a16a932a6394ed189cd4d5a14a9e45d
ennjamusic <ennjamusic@gmail.com>
2017-07-19 15:50:27 +0700

    15

--- mail-registration/src/main/resources/AddEmail.java
+++ mail-registration/src/main/resources/AddEmail.java
@@ -9,11 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddEmail
  */
+@WebServlet("/AddEmail")
 public class AddEmail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String data;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("name");
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
 		
+		
+		out.println(data);
 	}
 
 }
